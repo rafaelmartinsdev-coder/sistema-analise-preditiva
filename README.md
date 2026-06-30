7d8e235536a63689b04c31ed18cf924fa1b22e71
Rafael Martins <Rmgc1997@outlook.com>
2026-06-30 10:56:29 -0300

    Versão inicial do Sistema de Análise Preditiva

---
+++ README.md
@@ -0,0 +1,39 @@
+* Sistema de Análise Preditiva
+
+* *  Sobre o projeto
+Este projeto está sendo desenvolvido em Python com o objetivo de analisar estatísticas de partidas de futebol e futuramente gerar previsões baseadas em dados historicos. A ideia é evoluir o sistema até que ele seja capaz de comparar as duas equipes e calcular probabilidades utilizando diferentes indicadores estatísticos.
+
+* * Tecnologias utilizadas
+
+- Python
+- Request
+- API-Footbaal
+
+* * Funcionalidades implimentadas 
+
+- busca de equipes pela API
+- Seleção da equipe desejada
+- Consulta das partidas
+- Cálculo de vitórias, empates e derrotas
+- Cálculo de gols marcados e sofridos
+- Cálculo de média de gols
+- Cálculo de aproveitamento
+
+---- 
+
+* * Proximas funcionalidades 
+
+- Comparação entre duas equipes
+- Análise de desempenho como mandante e visitante
+- Forma recente das equipes
+- Probabilidades de resultados
+- Dashboard com gráficos
+- Modelos preditivos 
+
+---
+
+* * Status
+
+--- Projeto em desenvolvimento 
+
+
---
+++ main.py
@@ -0,0 +1,147 @@
+import requests
+
+url = "https://v3.football.api-sports.io/teams"
+
+headers = {
+    "x-apisports-key": "9bc2879e2e949b1680c9f0581a5b3b48"
+}
+
+def buscar_time():
+ 
+ time = input("Digite o nome do time:")
+ return time
+
+time = buscar_time()
+
+params = {
+   "search":time
+}
+
+resposta = requests.get(url, headers=headers, params=params)
+
+dados = resposta.json()
+
+print("Resultados encontrados:", dados["results"])
+
+if dados["results"]>0:
+   for i, item in enumerate(dados["response"],start=1):
+      print(
+         f"{i} - {item['team']['name']} - ID: {item['team']['id']}"
+      )
+
+   escolha = int(input("Escolha o número do time:"))
+   time_escolhido= dados["response"][escolha-1]
+   id_time = time_escolhido["team"]["id"]
+   nome_time = time_escolhido["team"]["name"]
+   print("\n Time Selecionado:")
+   print(nome_time)
+   print("ID:", id_time)
+else:
+      print("Time não  encontrado.")
+
+url_jogos =  "https://v3.football.api-sports.io/fixtures"
+params_jogos = {
+   "team":id_time,
+   "season": 2023
+}
+
+resposta_jogos = requests.get (
+   url_jogos,
+   headers=headers,
+   params=params_jogos
+)
+dados_jogos = resposta_jogos.json()
+
+
+print("\nÚltimos jogos encontrados:")
+
+vitorias= 0
+empates= 0
+derrotas= 0
+
+gols_marcados = 0
+gols_sofridos = 0
+
+forma_recente = []
+
+for jogo in dados_jogos["response"]:
+ 
+ mandante=jogo ["teams"]["home"]["name"]
+ visitante = jogo["teams"]["away"]["name"]
+
+ gols_mandante = jogo["goals"]["home"]
+ gols_visitante = jogo["goals"]["away"]
+
+
+ print(f"{mandante} {gols_mandante} x {gols_visitante} {visitante}")
+
+ if mandante == nome_time:
+   gols_marcados += gols_mandante
+   gols_sofridos += gols_visitante
+
+
+   if gols_mandante > gols_visitante:
+      vitorias += 1
+      forma_recente.append("V")
+
+
+   elif gols_mandante == gols_visitante:
+      empates +=1
+      forma_recente.append("E")
+   
+   else:
+      derrotas +=1
+      forma_recente.append("D")
+
+   print(">>> O time está jogando em casa")
+
+
+ else:
+    gols_marcados += gols_visitante
+    gols_sofridos +=gols_mandante
+      
+      
+    if gols_visitante > gols_mandante:
+         vitorias +=1
+         forma_recente.append("V")
+
+    elif gols_visitante == gols_mandante:
+         empates += 1
+         forma_recente.append("E")
+      
+    else:
+         derrotas +=1
+         forma_recente.append("D")
+
+    print(">>>O time está jogando fora")
+
+total_jogos= vitorias + empates + derrotas
+pontos = (vitorias * 3) + empates
+max_pontos= total_jogos * 3
+aproveitamento = (pontos / max_pontos) * 100
+
+media_gols_marcados = gols_marcados / total_jogos
+media_gols_sofridos = gols_sofridos / total_jogos
+
+forma_5_jogos= forma_recente[:5]
+
+
+
+print("\nResumo")
+
+print("Total de Jogos:", total_jogos)
+print("Vitórias:", vitorias)
+print("Empates:", empates)
+print("Derrotas:", derrotas)
+
+print("Pontos Conquistados:", pontos)
+print(f"Aproveitamento: {aproveitamento:.2f}%")
+
+
+print(f"Média de gols marcados: {media_gols_marcados:.2f}")
+print(f"Média de gols sofridos: {media_gols_sofridos:.2f}")
+
+print("Gols Marcados:", gols_marcados)
+print("Gols Sofridos:", gols_sofridos)
+
+print("Ultimos 5 jogos:", " ".join(forma_5_jogos))
